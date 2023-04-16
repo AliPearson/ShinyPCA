d377df07c5dce3a06d52eb2aff3afdeb6388e7fc
AliPearson <64259586+AliPearson@users.noreply.github.com>
2023-04-16 11:36:36 -0400

    Update app.R

--- app.R
+++ app.R
@@ -4,33 +4,41 @@ library(plotly)
 library(tidyverse)
 library(clipr)
 
-######### Modify for your data
+#################### Modify to your data...
 pca_df <- readRDS("pca_df.rds")
 pca_cap_df <- readRDS("pca_cap_df.rds")
-num_pcs<-3
+num_pcs<-2
 datasets<-list("pca_df"=pca_df, "pca_cap_df"=pca_cap_df)
-######## 
+###################
 
 ui <- fluidPage(
-  selectizeInput("colour",
+  titlePanel("ShinyPCA"),
+  sidebarLayout(
+    sidebarPanel(
+      selectizeInput("data",
+                     label = "data",
+                     #choices = c("Shotgun"="1", "Capture"="2")
+                     choices = c(names(datasets))),
+      selectizeInput("colour",
                      label = "colours",
-                     choices = colnames(pca_df)[num_pcs:ncol(pca_df)]),
-  selectizeInput("data",
-                 label = "data",
-                 #choices = c("Shotgun"="1", "Capture"="2")
-                 choices = c(names(datasets))),
+                     choices = colnames(pca_df)[(num_pcs+1):ncol(pca_df)]),
+      selectizeInput("xaxis",
+                     label = "xaxis",
+                     choices = colnames(pca_df)[1:num_pcs]),
+      selectizeInput("yaxis",
+                     label = "yaxis",
+                     choices = colnames(pca_df)[1:num_pcs])
+    ),
     mainPanel(
-      plotlyOutput("plot", height = "800px")),
+      plotlyOutput("plot", height = "800px"),
       verbatimTextOutput("hover"),
-      #verbatimTextOutput("click"),
-      #verbatimTextOutput("brushing"),
-      #verbatimTextOutput("selecting"),
-      #verbatimTextOutput("brushed"),
       verbatimTextOutput("selected"),
       br(),
       actionButton("copy", "Copy"),
       downloadButton('downloadData', 'Download data')
+    )
   )
+)
 
 server <- function(input, output, session) {
   datasetInput <- reactive({
@@ -40,12 +48,17 @@ server <- function(input, output, session) {
   selected <- reactiveValues(selected=NA)
   
   output$plot <- renderPlotly({
-    p1 <-ggplotly(ggplot(datasetInput(), aes(x=pc1,y=pc2,colour=datasetInput()[,input$colour], text=datasetInput()[,input$colour])) +
-                   geom_point()+theme(legend.position='none')+ggtitle(input$data), tooltip=c("text"))
+    p1 <-ggplotly(ggplot(datasetInput(), aes(x=datasetInput()[,input$xaxis],y=datasetInput()[,input$yaxis],colour=datasetInput()[,input$colour], text=datasetInput()[,input$colour])) +
+                   geom_point()+xlab(input$xaxis)+ylab(input$yaxis)+theme(legend.position='none')+ggtitle(input$data), tooltip=c("text"))
     p1 %>% 
       layout(dragmode = "select") %>%
       event_register("plotly_selecting")
   })
+  
+  #output$hover <- renderPrint({
+   ## d <- event_data("plotly_hover")
+   # if (is.null(d)) "Hover events appear here (unhover to clear)" else d
+  #})
   
   output$selected <- renderPrint({
     d <- event_data("plotly_selected")
@@ -75,4 +88,5 @@ server <- function(input, output, session) {
   
 }
 
-shinyApp(ui, server, options = list(display.mode = "showcase"))
+shinyApp(ui, server)
+##################
